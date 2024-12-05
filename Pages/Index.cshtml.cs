10e303ed481167c2fa679974bf62dfa2018baebb
Birk Jefta Svensson <bis001@edu.zealand.dk>
2024-12-05 13:31:33 +0100

    logout

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -15,8 +15,8 @@ namespace Viber.Pages
         //TODO: RET TILBAG TIL (/"Home")
         public IActionResult OnGet()
         {
-            //return RedirectToPage("/Home");
-            return RedirectToPage("/MoodBoardPages/ViewMoodBoard", new { Id = 19 });
+            return RedirectToPage("/Home");
+            //return RedirectToPage("/MoodBoardPages/ViewMoodBoard", new { Id = 19 });
         }
     }
 }
