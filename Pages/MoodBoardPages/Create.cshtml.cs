a58613c8d4626f8467e8960c3bafedd97bd89e6f
CHKiens <44415938+CHKiens@users.noreply.github.com>
2024-12-11 15:41:32 +0100

    create mb validering

--- Pages/MoodBoardPages/Create.cshtml.cs
+++ Pages/MoodBoardPages/Create.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Newtonsoft.Json.Linq;
-using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 using Viber.Models;
@@ -12,10 +12,8 @@ namespace Viber.Pages.MoodBoardPages
 {
     public class CreateModel : PageModel
     {
-
         public IPrimaryTagService _primaryTagService { get; set; }
         public IMoodboardService moodboardService { get; set; }
-
         public ISubTagService _subtagService { get; set; }
         public IContentContainerService _contentContainerService { get; set; }
 
@@ -23,18 +21,22 @@ namespace Viber.Pages.MoodBoardPages
         public Moodboard Moodboard { get; set; } = new Moodboard();
 
         [BindProperty]
-
         public List<PrimaryTag> PrimaryTags { get; set; }
+
         [BindProperty]
+        [Required(ErrorMessage = "Please select a primary tag.")]
         public int PrimaryTag { get; set; }
 
         [BindProperty]
         public List<string> Subtags { get; set; }
+
         [BindProperty]
+        [Required(ErrorMessage = "Please enter at least one subtag.")]
         public string subtaginput { get; set; }
 
         [BindProperty]
         public List<string> ContainerType { get; set; }
+
         [BindProperty]
         public List<string> ContainerText { get; set; }
 
@@ -45,37 +47,40 @@ namespace Viber.Pages.MoodBoardPages
             _contentContainerService = contentContainerService;
             _subtagService = subTagService;
         }
+
         public void OnGet()
         {
-            int userId = HttpContext.Session.GetInt32("UserId") ?? 0; //userId bliver 0 i stedet for null hvis den ikke kan findes
+            int userId = HttpContext.Session.GetInt32("UserId") ?? 0; // userId becomes 0 instead of null if not found
             Moodboard.UserId = userId;
-            
+
             PrimaryTags = _primaryTagService.GetPrimaryTags();
-            
         }
 
         public IActionResult OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
             Moodboard.PrimaryTagId = PrimaryTag;
             moodboardService.CreateMoodboard(Moodboard);
             _subtagService.SplitSubtagInput(subtaginput, Moodboard.PrimaryTagId, Moodboard.MoodboardId);
-      
+
             _contentContainerService.AddContainersToMoodboard(ContainerType, ContainerText, Moodboard);
             moodboardService.UpdateMoodboard(Moodboard);
 
             return RedirectToPage("/MoodBoardPages/Edit", new { Id = Moodboard.MoodboardId });
-            
         }
 
         public IActionResult OnPostCreateContainer()
-         {
+        {
             ContentContainer container = new ContentContainer()
             {
                 MoodboardId = Moodboard.MoodboardId
             };
             Moodboard.ContentContainers.Add(container);
             return RedirectToPage();
-         }
-
+        }
     }
 }
